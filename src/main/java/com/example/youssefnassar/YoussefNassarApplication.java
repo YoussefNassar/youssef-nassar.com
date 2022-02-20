93226ca308442340e1068a55eccd0babf7435b77
Youssef.Nassar <yossef41997@gmail.com>
2022-02-20 17:34:56 +0100

    init

---
+++ src/main/java/com/example/youssefnassar/YoussefNassarApplication.java
@@ -0,0 +1,13 @@
+package com.example.youssefnassar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoussefNassarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoussefNassarApplication.class, args);
+    }
+
+}
